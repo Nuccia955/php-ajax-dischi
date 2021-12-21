c585a1c6aaf486f2a3352e787af367a87f21d122
Nuccia955 <elenamunaro1995@gmail.com>
2021-12-21 14:49:39 +0100

    complete php version

--- php version/index.php
+++ php version/index.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . '/partials/scripts/data.php';?>
+<?php include __DIR__ . '/partials/scripts/database.php';?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,11 +6,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Dischi: php version</title>
 </head>
 <body>
-    <?php foreach($database as $disc) { ?> 
-        <img src="<?php echo $disc['poster']?>" alt="">
-    <?php } ?>
+    <!-- HEADER -->
+    <header>
+        <div class="logo">
+            <img src="./img/logo.png" alt="spotify logo">
+        </div>
+    </header>
+
+    <!-- MAIN -->
+    <main>
+        <div class="container">
+            <?php foreach($database as $disc) { ?> 
+                <div class="card">
+                    <div class="cover">
+                        <img src="<?php echo $disc['poster']?>" alt="album cover">
+                    </div>
+                    <div class="description">
+                        <h4 class="title">
+                            <?php echo $disc['title']?>
+                        </h4>
+                        <span class="author">
+                            <?php echo $disc['author']?>
+                        </span>
+                        <h4 class="year">
+                            <?php echo $disc['year']?>
+                        </h4>
+                        <span class="genre">
+                            <?php echo $disc['genre']?>
+                        </span>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </main>
 </body>
 </html>
