9539fac5a469ec00bbe26a4cc0ec01e3a9d66fbc
Nuccia955 <elenamunaro1995@gmail.com>
2021-12-21 15:43:55 +0100

    add AJAX version

--- ajax version/js/main.js
+++ ajax version/js/main.js
@@ -3,5 +3,20 @@ const app = new Vue({
     el: '#app',
     data: {
         discList: null,
+    },
+    created() {
+        this.getDiscList()
+    },
+    methods: {
+        getDiscList() {
+            axios.get('http://localhost/php-ajax-dischi/ajax%20version/partials/scripts/database.php')
+            .then(response => {
+                console.log(response.data);
+                this.discList = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
     }
 })
